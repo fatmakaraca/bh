844875b6a733576417c635abb024bd8cb48f2f43
Nehir Selinay Günenç <nehirgunenc06@gmail.com>
2025-08-06 20:53:50 +0300

    Add files via upload

---
+++ mobile_app/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\LearnFlutter\\btk_demo_c\\btk_demo" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter\\flutter"
+  "PROJECT_DIR=C:\\LearnFlutter\\btk_demo_c\\btk_demo"
+  "FLUTTER_ROOT=C:\\flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\LearnFlutter\\btk_demo_c\\btk_demo\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\LearnFlutter\\btk_demo_c\\btk_demo"
+  "FLUTTER_TARGET=C:\\LearnFlutter\\btk_demo_c\\btk_demo\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzIuOA==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049ZWRhZGE3YzU2ZQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049ZWYwY2QwMDA5MQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My44LjE="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\LearnFlutter\\btk_demo_c\\btk_demo\\.dart_tool\\package_config.json"
+)
